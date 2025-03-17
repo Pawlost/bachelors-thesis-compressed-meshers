00e7fa7a7054feb36ae152a1c3d3e83baf26f6b0
Pavel Balusek <mr.p.balusek@gmail.com>
2025-03-17 01:05:03 +0100

    Add Initial project structure with initial plugin import

---
+++ Source/CompressedMesherDemo/Test.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "Voxels/Voxel.h"
+#include "Test.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMPRESSEDMESHERDEMO_API UTest : public UObject
+{
+	GENERATED_BODY()
+
+	FVoxel voxel;
+};
